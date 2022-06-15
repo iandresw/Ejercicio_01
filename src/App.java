3ffe9b09badd93ab897e9b9245641a2300cedf1e
Andres Bejarano <rbejarano@unah.hn>
2022-06-14 21:36:03 -0600

    se ha modificado el main

--- src/App.java
+++ src/App.java
@@ -6,6 +6,21 @@ public class App {
     }
 
     public static void inicio(){
+        Seccion a = new Seccion();
+        Listado sa = new Listado();
+        a.setNombre("programacion");
+        a.setAula("106");
+        a.setHora("1600");
+        a.setuV("5");
+        sa.insertarPrimero("Pedro");
+        sa.insertarPrimero("Juan");
+        sa.insertarPrimero("maria");
+        a.setCupos(10);
+        sa.insertarPrimero("bety");
+
+        System.out.println(a.getNombre());
+        System.out.println(sa.toString());
         
     }
+    
 }
